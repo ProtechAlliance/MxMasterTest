99601a009a6cf772a79b72540576e89cd22b8a2e
ProtechAlliance <michael.becker@protechassociates.com>
2020-12-08 10:52:05 -0500

    Commit

--- b2121-MyAccount-MyCompany/Main.js
+++ b2121-MyAccount-MyCompany/Main.js
@@ -4,7 +4,7 @@ function Test(params)
 {
 
 
-	RVL.DoPlayScript("%WORKDIR%\\MxNavigation\\b2121-MyAccount-MyCompany\\Main.rvl.xlsx", Tester.GetParam("sheetName", "RVL"));
+	RVL.DoPlayScript("%WORKDIR%\\2121-MyAccount-MyCompany\\Main.rvl.xlsx", Tester.GetParam("sheetName", "RVL"));
 }
 
 g_load_libraries=["%g_browserLibrary%"]
