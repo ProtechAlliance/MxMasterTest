ffcea81643ef3c33a1ccc2cd769dc009c9d0fd1b
ProtechAlliance <michael.becker@protechassociates.com>
2020-12-13 11:58:15 -0500

    First Commit

---
+++ DeleteTest6/Objects.js
@@ -0,0 +1,80 @@
+var saved_script_objects={
+	"Get_Involved": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Get Involved",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Get Involved",
+		"xpath": "//li[3]/a",
+		"title": "Get Involved",
+		"url": "https://mx365qa84.ptassoc.com/Get-Involved"
+	},
+	"Our_Chapters": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Our Chapters",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Get Involved",
+		"xpath": "//div[1]/div/div/p[1]/a",
+		"title": "Get Involved",
+		"url": "https://mx365qa84.ptassoc.com/Get-Involved"
+	},
+	"Get_Involved1": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Get Involved",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Our Chapters",
+		"xpath": "//li[3]/a",
+		"title": "Our Chapters",
+		"url": "https://mx365qa84.ptassoc.com/Get-Involved/Our-Chapters"
+	}
+};
