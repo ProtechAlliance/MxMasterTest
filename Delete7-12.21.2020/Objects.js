fe5e93e870cdd4aeb5014de6db7d9e56e29c8f4a
ProtechAlliance <michael.becker@protechassociates.com>
2020-12-21 09:41:24 -0500

    test

---
+++ Delete7-12.21.2020/Objects.js
@@ -0,0 +1,54 @@
+var saved_script_objects={
+	"Events": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Events",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "Join",
+		"xpath": "//li[5]/a",
+		"title": "Join",
+		"url": "http://mx365dev.ptassoc.com/Membership/Join"
+	},
+	"Calendar_of_Events": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Calendar of Events",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "Events",
+		"xpath": "//div[1]/div/div/p[1]/a",
+		"title": "Events",
+		"url": "http://mx365dev.ptassoc.com/Events"
+	}
+};
