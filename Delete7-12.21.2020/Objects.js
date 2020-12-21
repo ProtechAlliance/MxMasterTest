6e696003ab090579ea62a8e6e14e5bfc20496f8b
ProtechAlliance <michael.becker@protechassociates.com>
2020-12-21 09:57:21 -0500

    test

--- Delete7-12.21.2020/Objects.js
+++ Delete7-12.21.2020/Objects.js
@@ -50,5 +50,31 @@ var saved_script_objects={
 		"xpath": "//div[1]/div/div/p[1]/a",
 		"title": "Events",
 		"url": "http://mx365dev.ptassoc.com/Events"
+	},
+	"Name": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "\n                                Name\n                            ",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Calendar of Events",
+		"xpath": "//input[@name='dnn$ctr1018$Find$ctl00$uscSearchForm$pnlFields$i0$i0$rptCols$ctl00$txt' and @type='text']",
+		"title": "Calendar of Events",
+		"url": "http://mx365dev.ptassoc.com/Events/Calendar-of-Events"
 	}
 };
