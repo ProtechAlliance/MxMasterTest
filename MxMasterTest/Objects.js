16979d834a10b4d882cff01dfb9b2cd924745860
ProtechAlliance <michael.becker@protechassociates.com>
2020-12-31 13:50:40 -0500

    Standard

--- MxMasterTest/Objects.js
+++ MxMasterTest/Objects.js
@@ -1471,32 +1471,6 @@ var saved_script_objects={
 		"object_library": "Spreadsheet",
 		"path": "%WORKDIR%\\Product Catalogue Purchase\\Invoices.xlsx"
 	},
-	"Knowledge_Base": {
-		"locations": [
-			{
-				"locator_name": "HTML",
-				"location": {
-					"xpath": "param:xpath",
-					"url": "param:url",
-					"title": "param:title"
-				}
-			}
-		],
-		"window_class": "Chrome_WidgetWin_1",
-		"object_text": "Chrome Legacy Window",
-		"object_role": "ROLE_SYSTEM_WINDOW",
-		"object_class": "Chrome_RenderWidgetHostHWND",
-		"version": 0,
-		"object_type": "HTMLObject",
-		"object_flavor": "Link",
-		"object_name": "Knowledge Base",
-		"ignore_object_name": true,
-		"object_library": "Chrome HTML",
-		"window_name": "Rapise Installed Successfully - RapiseDocs",
-		"xpath": "//div/nav/ul/li[9]/a",
-		"title": "Rapise Installed Successfully - RapiseDocs",
-		"url": "https://rapisedoc.inflectra.com/oninstall/"
-	},
 	"Last_Name": {
 		"locations": [
 			{
