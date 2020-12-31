e9cd47d2cb18b471c2f9bfc7a9304e63862adbd6
ProtechAlliance <michael.becker@protechassociates.com>
2020-12-31 14:51:53 -0500

    s

--- MxMasterTest/Objects.js
+++ MxMasterTest/Objects.js
@@ -3064,5 +3064,31 @@ var saved_script_objects={
 		"xpath": "//div[2]/div/div[2]/a",
 		"title": "My Profile",
 		"url": "http://mx365dev.ptassoc.com/My-Account/My-Profile"
+	},
+	"Identify_My_Company": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Generic",
+		"object_name": "Identify My Company",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Member",
+		"xpath": "//div[2]/h3",
+		"title": "Member",
+		"url": "http://mx365dev.ptassoc.com/Membership/Member"
 	}
 };
